00f280542c88682f290f7fa8337c424b9dda8ed4
Marija Milosevic <80062620+marmilosev@users.noreply.github.com>
2023-04-14 16:29:49 +0200

    Create Pomocno.java

---
+++ Pomocno.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VjezbaKolokvijTrosak;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author marija
+ */
+public class Pomocno {
+    
+    public static Scanner ulaz;
+    
+    public static String ucitajString(String poruka) {
+        String s=new String();
+        while(true){
+            System.out.println(poruka);
+            s = Pomocno.ulaz.nextLine();
+            if(!s.trim().isBlank()){
+                return s;
+            }
+            System.out.println("Obavezan unos");
+        }
+    }
+    
+   public static int ucitajInt(String poruka){
+       int i=0;
+       while(true){
+           System.out.println(poruka);
+           try {
+               i=Integer.parseInt(Pomocno.ulaz.nextLine());
+               if(i<0){
+                   System.out.println("Broj mora biti pozitivan");
+               }else{
+                   break;
+               }
+           } catch (Exception e) {
+               System.out.println("greška");
+           }
+           
+       }
+       return i;
+       
+   } 
+   
+   public static Date ucitajDatum(String poruka){
+       SimpleDateFormat sdf = 
+                new SimpleDateFormat(
+                        "dd.MM.yyyy.",
+                        new Locale("hr","HR")
+                );
+       for(;;){
+           try {
+               System.out.println(poruka);
+               return sdf.parse(Pomocno.ulaz.nextLine());
+           } catch (Exception e) {
+               System.out.println("Unosa datuma obavezno u formatu dd.MM.yyyy.");
+           }
+       }
+   }
+   
+  public static boolean ucitajBoolean(String poruka) {
+    while (true) {
+        System.out.println(poruka);
+        String input = Pomocno.ulaz.nextLine();
+        if (input.equalsIgnoreCase("true")) {
+            return true;
+        } else if (input.equalsIgnoreCase("false")) {
+            return false;
+        } else {
+            System.out.println("Neispravan unos, unijeti true ili false");
+        }
+    }
+}
+   
+   public static float ucitajFloat(String poruka) {
+        while (true) {
+            System.out.print(poruka);
+            try {
+                return Float.parseFloat(Pomocno.ulaz.nextLine());
+            } catch (Exception e) {
+                System.out.println("Decimalni broj s točkom");
+            }
+        }
+    }
+    
+}
